84443ad092dbf9c3bc046378b4c4fa27d661a9f4
Dylan Boyd <dylanjboyd@icloud.com>
2020-04-08 14:16:47 +1200

    Use footer value in translated JSON

--- translator.js
+++ translator.js
@@ -25,7 +25,7 @@ const translate = (title, description, footer, imageUrl, avatarUrl) => {
     };
 
     if (footer) result.footer = {
-        "text": `Movie added`,
+        "text": footer
     };
 
     return (result);
