89d53996e4520dc087083f63b73358ed6a864b8c
Dylan Boyd <dylanjboyd@icloud.com>
2020-04-08 14:01:19 +1200

    Add Radarr and Sonarr avatar images

--- translator.js
+++ translator.js
@@ -1,14 +1,14 @@
 const responseUsername = 'Plex';
 const appUrl = `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`;
 
-const translateMovie = payload => translate(`${payload.movie.title} (${payload.remoteMovie.year})`, 'Movie added', 'Movie added', null);
+const translateMovie = payload => translate(`${payload.movie.title} (${payload.remoteMovie.year})`, 'Movie added', 'Movie added', null, `${appUrl}/radarr-icon.png`);
 
-const translateShow = payload => translate(`${payload.series.title} (S${payload.episodes[0].seasonNumber.toString().padStart(2, '0')}E${payload.episodes[0].episodeNumber.toString().padStart(2, '0')})${payload.episodes.length > 1 ? '+' + (payload.episodes.length - 1) + 'others' : ''}`, 'Episode(s) added', 'Episode(s) added', null);
+const translateShow = payload => translate(`${payload.series.title} (S${payload.episodes[0].seasonNumber.toString().padStart(2, '0')}E${payload.episodes[0].episodeNumber.toString().padStart(2, '0')})${payload.episodes.length > 1 ? '+' + (payload.episodes.length - 1) + 'others' : ''}`, 'Episode(s) added', 'Episode(s) added', null, `${appUrl}/sonarr-icon.png`);
 
-const translate = (title, description, footer, imageUrl) => ({
+const translate = (title, description, footer, imageUrl, avatarUrl) => ({
     "content": '',
     "username": responseUsername,
-    "avatar_url": appUrl + '/plex-icon.png',
+    "avatar_url": avatarUrl,
     "embeds": [
         {
             "title": title,
