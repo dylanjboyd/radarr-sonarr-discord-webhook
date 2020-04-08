58c64b67299ab07a31e7e51afce11a8e02030d2b
Dylan Boyd <dylanjboyd@icloud.com>
2020-04-08 13:16:08 +1200

    Fix string padding bug for shows

--- translator.js
+++ translator.js
@@ -3,7 +3,7 @@ const appUrl = `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`;
 
 const translateMovie = payload => translate(`${payload.movie.title} (${payload.remoteMovie.year})`, 'Movie added', 'Movie added', null);
 
-const translateShow = payload => translate(`${payload.series.title} (S${payload.episodes[0].seasonNumber.padStart(2, '0')}E${payload.episodes[0].episodeNumber.padStart(2, '0')})${payload.episodes.length > 1 ? '+' + (payload.episodes.length - 1) + 'others' : ''}`, 'Episode(s) added', 'Episode(s) added', null);
+const translateShow = payload => translate(`${payload.series.title} (S${payload.episodes[0].seasonNumber.toString().padStart(2, '0')}E${payload.episodes[0].episodeNumber.toString().padStart(2, '0')})${payload.episodes.length > 1 ? '+' + (payload.episodes.length - 1) + 'others' : ''}`, 'Episode(s) added', 'Episode(s) added', null);
 
 const translate = (title, description, footer, imageUrl) => ({
     "content": '',
